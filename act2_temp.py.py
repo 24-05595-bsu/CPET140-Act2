87dee0c4c12ddcd53190127014a513b1a50fd96f
24-05595-bsu <24-05595@g.batstate-u.edu.ph>
2026-09-10 23:40:15 +0800

    Add files via upload

---
+++ act2_temp.py.py
@@ -0,0 +1,11 @@
+#julius Mendoza
+#BAET2B
+
+
+celsius = float(input("Enter temperature in °C: "))
+
+fahrenheit = celsius * 9 / 5 + 32
+between = 20 <= celsius <= 30
+
+print("Fahrenheit:", fahrenheit)
+print("Between 20 and 30 °C:", between)
